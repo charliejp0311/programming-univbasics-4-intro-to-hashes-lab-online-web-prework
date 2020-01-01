08cc3946eb4ebd8db8b571de9e8b7247e951d383
Charles Pelton <charliejp0311@gmail.com>
2020-01-01 20:12:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,9 +42,9 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   num = 1 
   if hash[key]
-    hash[key] += num 
+    p hash[key] += num 
   else
-    hash[key] = num
+    p hash[key] = num
   end
   
 end
